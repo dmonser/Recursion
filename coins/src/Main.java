a40c7b17abcd055ca31bd6f4ca1c3651bb92a7d9
Роман Носов <romannosov_2001@mail.ru>
2022-09-05 19:53:47 +0300

    Added 'check' (not working)

--- coins/src/Main.java
+++ coins/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String answer = check(10);
+        System.out.println(answer);
+    }
+
+    public static String check (int sum){       // TODO: 05.09.2022 Not working
+        if (sum >= 3 && check(sum - 3).equals("да")){
+            return "да";
+        }
+        if (sum >= 5 && check(sum - 5).equals("да")){
+            return "да";
+        }
+        return "нет";
     }
 }
